71121af2503e45abcbdf043a1de1ac2877f25f6a
Bryan Ngo <bngo@ucdavis.edu>
2017-04-18 17:49:05 -0700

    added method to get API key from local machine

--- src/main.java
+++ src/main.java
@@ -5,13 +5,16 @@ import com.robrua.orianna.type.core.currentgame.Participant;
 import com.robrua.orianna.type.core.league.League;
 import com.robrua.orianna.type.core.summoner.Summoner;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 class Main {
     public static void main(String[] args) {
 
-        String APIKEY = "cda032a8-f14b-437d-a16d-e3941668ba5f";
-        String summonerName = "Lunoski";
+        String APIKEY = getBryanAPIKey("League");
+        String summonerName = "BubblyBryan";
 
 
         RiotAPI.setRegion(Region.NA);
@@ -21,14 +24,13 @@ class Main {
         CurrentGame currentGame = summoner.getCurrentGame();
         List<Participant> participants = currentGame.getParticipants();
 
-        final List<League> leagues;
         List<League> leaguesBySummonerName;
 
         String tier, champ;
 
 
         for(int i = 0; i < 10; i++) {
-            leaguesBySummonerName = RiotAPI.getLeaguesBySummonerName(participants.get(i).getSummonerName());
+            leaguesBySummonerName = RiotAPI.getLeaguesBySummonerID(participants.get(i).getSummonerID());
             champ = participants.get(i).getChampion().getName();
             tier = leaguesBySummonerName.get(0).getTier().toString();
 
@@ -36,4 +38,44 @@ class Main {
         }
 
     }
+
+    private static String getBryanAPIKey(String APIcompany) {
+
+        // holds the list of api keys
+        List<String> records = new ArrayList<String>();
+
+        // open the file and add the keys to the list
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader("/Users/Bryan1/keys.text"));
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                records.add(line);
+            }
+            reader.close();
+        }
+
+        // error
+        catch (Exception e)
+        {
+            System.err.format("Exception occurred trying to read '%s'.", "/Users/Bryan1/keys.text");
+            e.printStackTrace();
+            return null;
+        }
+
+        // return the requested api key
+        for (String record : records)
+            if (record.contains(APIcompany)) {
+                String[] tokens = record.split("=");
+                return tokens[1];
+            }
+
+
+        return "ERROR";
+
+    }
+
+} // returns an APIKey.
+
 }
